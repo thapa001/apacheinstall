e7d69d2033fa6b1a16bc463396346c3730aa25eb
thapa001 <43660000+thapa001@users.noreply.github.com>
2018-10-05 00:22:06 -0700

    Update Apache.sh

--- Apache.sh
+++ Apache.sh
@@ -3,4 +3,4 @@ yum -y install httpd mod_ssl
 systemct1 enable httpd
 systemctl start  httpd
 sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf
-echo "<html><body><h1>>h1 there</h1></body</html>" > /var/www/html/index.html
+echo "<html><body><h1>hi there</h1></body</html>"> /var/www/html/index.html
